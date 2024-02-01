f20f5f70b6cd819f92b80fb392eb6d8cc77bc0f4
VIvek Halder <vivekh.it.ug@jadavpuruniversity.in>
2024-02-01 18:03:26 +0530

    router dom added

--- Front End/appname/src/App.jsx
+++ Front End/appname/src/App.jsx
@@ -1,13 +1,28 @@
-import React from "react"
-import Home from "./HomeComponents/home"
-import Authentication from "./HomeComponents/Authentication"
+import React from "react";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home, Login, Register } from "../src/HomeComponents";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  }
+]);
+
 function App() {
- 
   return (
    <div className=" h-screen w-screen flex justify-center items-center">
-      <Home></Home>
+      <RouterProvider router={router}/>
    </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Front End/appname/src/HomeComponents/index.js
@@ -0,0 +1,11 @@
+import Authentication from "./Authentication";
+import Content from "./Content";
+import Home from "./Home";
+import Login from "./Login";
+import LoginMessage from "./LoginMessage";
+import Navbar from "./Navbar";
+import Register from "./Register";
+import RegisterMessage from "./RegisterMessage";
+import Transition from "./Transition";
+
+export { Authentication, Content, Home, Login, LoginMessage, Navbar, Register, RegisterMessage, Transition };
